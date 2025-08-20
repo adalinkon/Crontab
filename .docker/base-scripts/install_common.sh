35fe5a3c55af2ad65b74db1a217d0279a3c57f47
adalinkon <dh1608958498@gmail.com>
2025-08-21 04:53:11 +0800

    fix

--- .docker/base-scripts/install_common.sh
+++ .docker/base-scripts/install_common.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "running install_common.sh"
 package_list="git neovim sudo gnupg2 bash-completion curl wget locales lsof ca-certificates \
-    unzip bzip2 xz-utils zip unzip zlib1g tar \
+    unzip bzip2 xz-utils zip zlib1g tar \
     iproute2 net-tools apt-transport-https dirmngr psmisc procps apt-utils\
     libstdc++6 libc6 libicu[0-9][0-9] libgcc1 pkg-config ninja-build\
     openssh-server aria2 \
